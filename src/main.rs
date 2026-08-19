e5135e403da8ccbb7b31ff1f5d56aa290b89604f
Daniel Chiquito <daniel.chiquito@gmail.com>
2026-08-19 11:27:19 -0400

    File arg

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use adventus4::{compile::ByteCode, vm::VM};
 
 fn main() {
-    let source_code = "def mean:(int int -> int)  [+ 2 /]
-        def new ?(-> int int) [1 2]
-        def swip [
-            >$a
-            >$b
-            $a $b
-        ]
-        def main [10 20 swip / print]";
-    let bytecode = ByteCode::compile(source_code);
-    eprintln!("{bytecode:?}");
-    let mut vm = VM::new(bytecode);
-    vm.run();
+    if let Some(in_file) = std::env::args().nth(1) {
+        let source_code = std::fs::read_to_string(in_file).unwrap();
+        let bytecode = ByteCode::compile(&source_code);
+        eprintln!("{bytecode:?}");
+        let mut vm = VM::new(bytecode);
+        vm.run();
+    }
     // let root_node = tree.root_node();
     // eprintln!("{root_node:?}")
 }
